2ced96c08976e4157559d98caf0cbd96f529a997
dsh7x <dshx7x@gmail.com>
2025-02-16 12:02:59 +0300

    Added getBookInfo.kt

---
+++ Tasks/getBookInfo.kt
@@ -0,0 +1,16 @@
+class Book(val title: String, val author: String,
+           val yearPublished: Int) {
+    fun getBookInfo(): String {
+        return "📖 $title by $author ($yearPublished)"
+    }
+}
+
+fun main() {
+    val book1 = Book(
+        "The Awakening",
+        "L.J.Smith",
+        1991
+    )
+
+    println(book1.getBookInfo())
+}
